d31312948770f1358181f9f4ebe9bc719755c382
Suphawit2004 <tiwsuphawit1@gmail.com>
2026-01-15 02:22:43 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -377,7 +377,7 @@ export default function Home() {
 
       <div className="flex gap-3">
         <button
-          className="px-4 py-2 rounded bg-gray text-white"
+          className="px-4 py-2 rounded bg-gray-300 text-white"
           onClick={startCamera}
         >
           Start Camera
